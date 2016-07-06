51f80fac02c8ee8e2902a2f5158a16d75c07aa66
Jack Shen <jack.shen@tpgtelecom.com.au>
2016-07-06 11:42:45 +1000

    add a wrong testcase

--- src/test/java/ci/TestCustomer.java
+++ src/test/java/ci/TestCustomer.java
@@ -7,8 +7,12 @@ import org.junit.Test;
 public class TestCustomer {
 
 	@Test
-	public void test() {
+	public void test01() {
 		assertEquals("Hello Jack!", new Customer().sayHello());
 	}
 
+	@Test
+	public void test02() {
+		assertEquals("Hello Jack", new Customer().sayHello());
+	}
 }
