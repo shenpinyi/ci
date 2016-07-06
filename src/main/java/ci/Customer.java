7cbe528568152432a734d40b5b7f3962c98bcb97
Jack Shen <jack.shen@tpgtelecom.com.au>
2016-07-06 15:29:59 +1000

    add a findbugs warning

--- src/main/java/ci/Customer.java
+++ src/main/java/ci/Customer.java
@@ -1,12 +1,18 @@
 package ci;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Customer {
 
 	public String sayHello() {
 		
+		List l = new ArrayList();
+		
 		if (1 != 1) {
 			return "Hello God!";
 		}
+		
 		
 		return "Hello Jack!";
 	}
