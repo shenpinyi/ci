20e0a31c1ba5044a3bbe3df048debf43f2d139b0
Jack Shen <jack.shen@tpgtelecom.com.au>
2016-07-06 15:47:10 +1000

    modify code back to make code check better

--- src/main/java/ci/Customer.java
+++ src/main/java/ci/Customer.java
@@ -1,19 +1,9 @@
 package ci;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Customer {
 
 	public String sayHello() {
-		
-		List l = new ArrayList();
-		
-		if (1 != 1) {
-			return "Hello God!";
-		}
-		
-		
+
 		return "Hello Jack!";
 	}
 	
